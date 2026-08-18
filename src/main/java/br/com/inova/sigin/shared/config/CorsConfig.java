18b0081059d400da2853b29bc62a75ab58b4f8d2
Rick <ricrdocardoso@gmail.com>
2026-08-18 00:05:55 -0300

    fix(core): allow admin frontend cors origin

--- src/main/java/br/com/inova/sigin/shared/config/CorsConfig.java
+++ src/main/java/br/com/inova/sigin/shared/config/CorsConfig.java
@@ -17,7 +17,8 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "https://sigin-admin.vercel.app/"
         ));
 
         config.setAllowedMethods(List.of(
